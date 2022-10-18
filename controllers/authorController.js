68659217a1e35b992df977ff8e55d9a2ff45818d
Parker Fisher <pfishe13@kent.edu>
2022-10-18 12:52:16 -0400

    Added controllers and routes

---
+++ controllers/authorController.js
@@ -0,0 +1,41 @@
+const Author = require('../models/author');
+
+// Display list of all authors
+exports.author_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author List');
+};
+
+// Display Detail page for a specific Author
+exports.author_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Author Detail: ${req.params.id}`);
+};
+
+// Display Author Create form on GET
+exports.author_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create GET');
+};
+
+// Handle Author create on POST
+exports.author_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
+};
+
+// Display Author delete form on GET.
+exports.author_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+// Handle Author delete on POST.
+exports.author_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+// Display Author update form on GET.
+exports.author_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+// Handle Author update on POST.
+exports.author_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update POST');
+};
