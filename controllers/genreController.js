f412bf13f51a162da3179e55bb1391075885d2b1
Parker Fisher <pfishe13@kent.edu>
2022-10-24 12:03:54 -0400

    Add ability to edit genre object

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -167,10 +167,47 @@ exports.genre_delete_post = (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.genre == null) {
+        const err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render('genre_form', {
+        title: 'Update Genre',
+        genre: results.genre,
+      });
+    }
+  );
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(thegenre.url);
+    });
+  },
+];
